8b18d03d4a1a66686ded22db343f0903f7e46114
GreenSeaHawk <andrew.boyce96@hotmail.com>
2024-11-11 16:04:49 +0000

    fix naming error in iam role

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,7 +3,7 @@
 resource "aws_lambda_function" "extract_lambda_func" {
     function_name = "extract_lambda_func"
     filename = "${path.module}/../lambda/extract/handler.py" #change depending on lambda extract file
-    role = aws_iam_role.lambda_role.arn
+    role = aws_iam_role.extract_lambda_role.arn
     handler = "handler.lambda_handler" # change depending on lambda extract file
     runtime = "python3.9"
     timeout = 10
