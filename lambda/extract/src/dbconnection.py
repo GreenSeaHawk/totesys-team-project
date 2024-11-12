e30fedb51297bddddb7578b6f9f948e3cc8840dc
jamil <mohammed.ahmed.34@city.ac.uk>
2024-11-12 10:10:18 +0000

    completed all tests for dbconnection

--- lambda/extract/src/dbconnection.py
+++ lambda/extract/src/dbconnection.py
@@ -7,7 +7,6 @@ from botocore.exceptions import ClientError
 def get_db_credentials(secret_name = "my-database-connection"):
     """get the credentials from the secret manager"""
     my_client = boto3.client("secretsmanager", region_name="eu-west-2")
-    # secret_name = "my-database-connection"
     try:
         response = my_client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
@@ -26,13 +25,14 @@ def connect_to_db():
     credentials = get_db_credentials()
     if not credentials:
         print("Failed to retrieve database credentials.")
+        return None
     try:
         conn = pg8000.connect( user= credentials["user"], 
             password = credentials["password"], 
             host= credentials["host"], 
             database= credentials["database"], 
             port= credentials["port"])
-        print("Database connection successful....")
+       
         return conn
     except pg8000.DatabaseError as de:
         raise de
@@ -40,38 +40,3 @@ def connect_to_db():
 def close_db_connection(conn):
     conn.close()
 
-def get_secret():
-
-    secret_name = "my-database-connection"
-    region_name = "eu-west-2"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        raise e
-
-    secret = get_secret_value_response['SecretString']
-
-    if "SecretString" in get_secret_value_response:
-        secret = get_secret_value_response["SecretString"]
-        secret_dict = json.loads(secret) # json string to dictionary
-        return secret_dict
-    else:
-        pass
-
-# credentials = get_secret()
-# if credentials:
-#     print("Database User", credentials["user"]) # e.g. username
-#     print("Database Password", credentials["password"])
-#     print("Host", credentials["host"])
-#     print("Database Name", credentials["database"])
-#     print("Database Port", credentials["port"])
--- lambda/extract/tests/test_dbconnection.py
+++ lambda/extract/tests/test_dbconnection.py
@@ -1,10 +1,11 @@
 import pytest
 import json
 import boto3
-from src.dbconnection import get_db_credentials
+from src.dbconnection import get_db_credentials, connect_to_db
 from moto.secretsmanager import mock_secretsmanager
 from botocore.exceptions import ClientError
-
+from pg8000 import DatabaseError
+from unittest.mock import patch, MagicMock
 
 # Initialize mock Secrets Manager
 @pytest.fixture
@@ -44,3 +45,48 @@ def test_get_db_credentials_with_secret_value_not_found(secrets_client):
 
     # Assert
     assert result == "The Secret name could not be found, please check."
+
+
+@patch("src.dbconnection.get_db_credentials")
+@patch("src.dbconnection.pg8000.connect")
+def test_connect_to_db_success(mock_connect, mock_get_db_credentials):
+    mock_get_db_credentials.return_value = {
+            'user': "test_user", 
+            'password' : "test_password", 
+            'host': "test_host", 
+            'database' : "test_database", 
+            'port' : 1000
+    }
+    mock_conn = MagicMock()
+    mock_connect.return_value = mock_conn
+    conn = connect_to_db()
+    mock_connect.assert_called_once_with(user= "test_user", 
+            password = "test_password", 
+            host= "test_host", 
+            database = "test_database", 
+            port = 1000)
+    assert conn == mock_conn
+
+@patch("src.dbconnection.get_db_credentials")
+@patch("src.dbconnection.pg8000.connect")
+def test_connect_to_db_when_no_credentials(mock_connect, mock_get_db_credentials, capsys):
+    mock_get_db_credentials.return_value = None
+    conn = connect_to_db()
+    captured = capsys.readouterr()
+    assert conn is None
+    assert "Failed to retrieve database credentials." in captured.out
+    mock_connect.assert_not_called()
+
+@patch("src.dbconnection.get_db_credentials")
+@patch("src.dbconnection.pg8000.connect")
+def test_connect_to_db_when_database_error(mock_connect, mock_get_db_credentials, capsys):
+    mock_get_db_credentials.return_value = {
+            'user': "test_user", 
+            'password' : "test_password", 
+            'host': "test_host", 
+            'database' : "test_database", 
+            'port' : 1000
+    }
+    mock_connect.side_effect=DatabaseError('Database connection failed')
+    with pytest.raises(DatabaseError, match="Database connection failed"):
+        connect_to_db()
