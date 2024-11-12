e0310ade436571dc53df0175dd27c9afc4fdc4e5
Nicholas <nick-rowley117@hotmail.com>
2024-11-12 14:32:47 +0000

    load iam, load lambda terraform added

---
+++ terraform/load - iam.tf
@@ -0,0 +1,8 @@
+#create extract lambda role, and attaches policy to role
+resource "aws_iam_role" "load_lambda_role" {
+    name = "role-lambda"
+    assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+
+#ADD permissions to upload transformed data to RDS warehouse
---
+++ terraform/load - lambda.tf
@@ -0,0 +1,52 @@
+# CHANGE FILE PATH NAMES WHEN KNOWN
+
+resource "aws_lambda_function" "load_lambda_func" {
+    function_name = "load_lambda_func"
+    filename = "${path.module}/../lambda/load/handler.py" #change depending on lambda load file
+    role = aws_iam_role.load_lambda_role.arn
+    handler = "handler.lambda_handler" # change depending on lambda load file
+    runtime = "python3.9"
+    timeout = 10
+    source_code_hash = data.archive_file.archive_load_lambda.output_base64sha256
+    layers = [aws_lambda_layer_version.load_lambda_layer.arn]
+}
+
+# Zip load lambda handler to local zip file
+data "archive_file" "archive_load_lambda" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/load/handler.py"
+  output_path = "${path.module}/../load_lambda_func.zip"
+}
+
+# Zip load layer requirements to local file
+data "archive_file" "load_requirements_layer" {
+    type = "zip"
+    source_dir = "${path.module}/../lambda/load/" #complete this when path to layer known
+    output_path = "${path.module}/../load_lambda_layer.zip"
+}
+
+# Attach zipped load layer to load lambda func
+resource "aws_lambda_layer_version" "load_lambda_layer" {
+  filename   = "${path.module}/../load_lambda_layer.zip"
+  layer_name = "load_lambda_layer"
+}
+
+
+# Trigger for load if something added to raw data bucket
+resource "aws_s3_bucket_notification" "aws_load_lambda_trigger" {
+  bucket = totesys_data_bucket.bucket.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.load_lambda_func.arn
+    events              = ["s3:ObjectCreated:*"]
+
+  }
+}
+
+# Permissions for s3 bucket to trigger load func
+resource "aws_lambda_permission" "allow_s3_bucket_to_execute_load" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.load_lambda_func.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${totesys_transformed_data_bucket.bucket.id}"
+}
