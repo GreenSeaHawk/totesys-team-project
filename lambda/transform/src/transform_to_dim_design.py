d23a1edd20bf38ac41b5b6f3b245767f221f9142
GreenSeaHawk <andrew.boyce96@hotmail.com>
2024-11-14 11:35:46 +0000

    fixed more typos

--- lambda/transform/src/transform_to_dim_design.py
+++ lambda/transform/src/transform_to_dim_design.py
@@ -6,7 +6,7 @@ def transform_to_dim_design(design_data):
         raise Exception('Error, design_data is empty')
     '''Simply have to convert it to a table with different
     names so have done this below'''
-    dim_location_entries = []
+    dim_design_entries = []
     for design in design_data:
         temp_dict = {
             "design_id": design["design_id"],
@@ -14,8 +14,8 @@ def transform_to_dim_design(design_data):
             "file_location": design["file_location"],
             "file_name": design["file_name"]
         }
-        dim_location_entries.append(temp_dict)
+        dim_design_entries.append(temp_dict)
     '''Have used separators to keep the format the same as the other tables
     where .to_json from pandas outputs without whitespace'''
-    return json.dumps(dim_location_entries, separators=(',',':'))
+    return json.dumps(dim_design_entries, separators=(',',':'))
 
--- lambda/transform/src/transform_to_dim_payment_type.py
+++ lambda/transform/src/transform_to_dim_payment_type.py
@@ -6,13 +6,13 @@ def transform_to_dim_payment_type(payment_type_data):
         raise Exception('Error, payment_type_data is empty')
     '''Simply have to convert it to a table with different
     names so have done this below'''
-    dim_location_entries = []
+    dim_payment_type_entries = []
     for payment_type in payment_type_data:
         temp_dict = {
             "payment_type_id": payment_type["payment_type_id"],
             "payment_type_name": payment_type["payment_type_name"]
         }
-        dim_location_entries.append(temp_dict)
+        dim_payment_type_entries.append(temp_dict)
     '''Have used separators to keep the format the same as the other tables
     where .to_json from pandas outputs without whitespace'''
-    return json.dumps(dim_location_entries, separators=(',',':'))
+    return json.dumps(dim_payment_type_entries, separators=(',',':'))
