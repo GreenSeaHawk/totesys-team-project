02cf9ca6dd1f40253dfd46833ff98c1e45e4cf4a
GreenSeaHawk <andrew.boyce96@hotmail.com>
2024-11-14 14:06:15 +0000

    started work on transform_to_fact_sales_order

---
+++ lambda/transform/src/transform_to_fact_sales_order.py
@@ -0,0 +1,13 @@
+
+
+'''Need data from sales_order, 
+
+created_at (timestamp) --> created_date (date: yyyy-mm-dd)
+created_at (timestamp) --> created_time (time: NN:NN:NN)
+last_updated (timestamp) --> last_updated_date (date: yyyy-mm-dd)
+agreed_delivery_date (varchar in form yyyy-mm-dd) 
+--> agreed_delivery_date (date: yyyy-mm-dd)
+agreed_payment_date (varchar in form yyyy-mm-dd) 
+--> agreed_payment_date (date: yyyy-mm-dd)
+'''
+
