a417a6ba14d75a1811de991ad3f6f32ea437f660
f1pm <fpothiyil@gmail.com>
2024-11-18 10:33:43 +0000

    tighten up restrictions on exctract function

--- terraform/extract - iam.tf
+++ terraform/extract - iam.tf
@@ -1,4 +1,12 @@
-#policy doc for lambda role
+
+#create extract lambda role, and attaches policy to role
+resource "aws_iam_role" "extract_lambda_role" {
+    name = "role-lambda"
+    assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+
+#trust policy doc for lambda role
 data "aws_iam_policy_document" "lambda_assume_role" {
     statement {
         effect = "Allow"
@@ -11,14 +19,8 @@ data "aws_iam_policy_document" "lambda_assume_role" {
         actions = ["sts:AssumeRole"]
     }
 }
-
-#create extract lambda role, and attaches policy to role
-resource "aws_iam_role" "extract_lambda_role" {
-    name = "role-lambda"
-    assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
-}
-
-# extract lambda policy document - putobject
+################################################################################################################################################################################################################################
+# s3 permissions
 data "aws_iam_policy_document" "s3_document" {
   statement {
 
@@ -32,6 +34,17 @@ data "aws_iam_policy_document" "s3_document" {
   }
 }
 
+resource "aws_iam_policy" "s3_policy" {
+    name_prefix = "s3-policy-extract_lambda_func"
+    policy = data.aws_iam_policy_document.s3_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
+    role = aws_iam_role.extract_lambda_role.name
+    policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+################################################################################################################################################################################################################################
 # cloudwatch policy document
 data "aws_iam_policy_document" "cw_document" {
   statement {
@@ -53,41 +66,37 @@ data "aws_iam_policy_document" "cw_document" {
   }
 }
 
-resource "aws_iam_policy" "s3_policy" {
-    name_prefix = "s3-policy-extract_lambda_func"
-    policy = data.aws_iam_policy_document.s3_document.json
-}
 
 resource "aws_iam_policy" "cw_policy" {
     name_prefix = "cw-policy-extract_lambda_func"
     policy = data.aws_iam_policy_document.cw_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
-    role = aws_iam_role.extract_lambda_role.name
-    policy_arn = aws_iam_policy.s3_policy.arn
-}
 
 resource "aws_iam_role_policy_attachment" "lambda_cw_policy_attachment" {
     role = aws_iam_role.extract_lambda_role.name
     policy_arn = aws_iam_policy.cw_policy.arn
 }
 
-
-resource "aws_iam_role_policy" "sm_policy" {
-  name = "sm_access_permissions"
-  role = aws_iam_role.extract_lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "secretsmanager:GetSecretValue",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+################################################################################################################################################################################################################################
+#Secrets manager policy
+data "aws_iam_policy_document" "extract_lambda_secrets_access" {
+    statement {
+        actions = ["secretsmanager:GetSecretValue"]
+       resources = ["arn:aws:secretsmanager:eu-west-2:039612847146:secret:my-database-connection-K3XcnO"]
+    }
 }
+
+
+resource "aws_iam_policy" "sm_policy" {
+  name_prefix = "sm_access_permissions"
+  policy = data.aws_iam_policy_document.extract_lambda_secrets_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "extract_lambda_sm_access" {
+    role = aws_iam_role.extract_lambda_role.name
+    policy_arn = aws_iam_policy.sm_policy.arn
+}
+################################################################################################################################################################################################################################
+
+
