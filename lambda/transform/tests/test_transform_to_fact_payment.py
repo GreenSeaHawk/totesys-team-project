7e7f03e0afcf3f29bbd2d916e2c6efe88c4fce93
GreenSeaHawk <andrew.boyce96@hotmail.com>
2024-11-19 16:59:38 +0000

    started fixing fact tables

--- lambda/transform/tests/test_transform_to_fact_payment.py
+++ lambda/transform/tests/test_transform_to_fact_payment.py
@@ -1,7 +1,33 @@
 import json
 import pytest
 from src.transform_to_fact_payment import transform_to_fact_payment
+from moto import mock_aws
+import boto3
+import os
 
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+@pytest.fixture(scope="function")
+def s3(aws_credentials):
+  """
+  Return a mocked S3 client
+  """
+  with mock_aws():
+    yield boto3.client("s3", region_name="eu-west-2")
+
+@pytest.fixture
+def create_data_bucket(s3):
+    s3.create_bucket(
+        Bucket="data_bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+    )
 
 payment_type_sample_data = [
   {
@@ -67,12 +93,28 @@ expected_output = [
   }
 ]
 
-def test_fact_payment_happy_case():
-    output = transform_to_fact_payment(payment_type_sample_data)
-    expected_json_output = json.dumps(expected_output, separators=(',',':'))
+def test_fact_payment_happy_case(create_data_bucket):
+  output = transform_to_fact_payment(payment_type_sample_data, "data_bucket")
+  expected_json_output = json.dumps(expected_output, separators=(',',':'))
 
-    assert output == expected_json_output
+  assert output == expected_json_output
 
-def test_returns_error_if_payment_data_is_empty():
-    with pytest.raises(Exception, match='Error, payment_data is empty'):
-        transform_to_fact_payment([])
+def test_returns_error_if_payment_data_is_empty(create_data_bucket):
+  with pytest.raises(Exception, match='Error, payment_data is empty'):
+    transform_to_fact_payment([])
+
+
+def test_count_starts_at_1_if_no_object_in_s3(s3):
+  transform_to_fact_payment(payment_type_sample_data)
+  
+# def test_count_saves_to_s3_bucket(s3, create_data_bucket):
+#   pass
+
+
+# def test_count_is_extracted_from_s3_bucket(s3, create_data_bucket):
+#   pass
+
+
+# def test_count_is_both_saved_and_extracted_from_s3_bucket(s3, create_data_bucket):
+#   pass
+
